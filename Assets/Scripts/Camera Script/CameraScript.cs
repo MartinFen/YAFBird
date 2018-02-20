632ae39a98de528d184faf3de4b35626162b68e0
Martin Fennell <MartinFen@users.noreply.github.com>
2018-02-20 21:12:18 +0000

    Large commit
    -Fixed bird flapping annimator issue
    -added more to the bird script
    -added a camera script still need to make it follow the bird

---
+++ Assets/Scripts/Camera Script/CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public static float offsetX;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    //checks if the bird is within the game bounds & if the bird is a live. if so the camera will move 
+	void Update () {
+
+        if (BirdScript.instance != null) {
+            if (BirdScript.instance.isAlive) {
+                MoveTheCamera();
+            }
+        }
+		
+	}
+    //function for the camer to follow the bird object
+    void MoveTheCamera() {
+        Vector3 temp = transform.position;
+        temp.x = BirdScript.instance.GetPositionX() + offsetX;
+        transform.position = temp;
+    }
+}
